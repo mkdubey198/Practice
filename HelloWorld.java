224449ccf7badb94f2b1f9bb46af91c234202d30
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-10 19:09:47 +0530

    HelloWorld
    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//package practice;
+
+public class HelloWorld {
+	
+	public static void main(String [] args)
+	{
+		System.out.println("This is First Program in Selenium - Git and jenkins");
+	}
+
+}
