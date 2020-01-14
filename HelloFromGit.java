26c8f8104ca5eece937eb61591d214fab9f32f86
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-14 12:34:46 +0530

    HelloFromGit.java_(v1.00)
    HelloFromGit.java_(v1.00)

---
+++ HelloFromGit.java
@@ -0,0 +1,10 @@
+  
+//This is Second File
+public class HelloFromGit {
+	
+	public static void main(String [] args)
+	{
+		System.out.println("This is Second Program - This is From Other Repository");
+		System.out.println("This is FISRT and Initial COMMIT in GitHub - HelloFromGit - Hello Jenkins From Git");
+	}
+  }
