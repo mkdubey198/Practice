121099dd5dd0bd680893940cc2ccebc6960cf80c
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-14 15:33:40 +0530

    HelloWorld.java_(v1.001)
    HelloWorld.java_(v1.001)

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,13 @@ public class HelloWorld {
 	public static void main(String [] args)
 	{
 		System.out.println("This is First Program in Selenium - Git and jenkins");
-		System.out.println("Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		System.out.println("Third Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+		System.out.println("FISRT Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+		/*
+		System.out.println("SECOND Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+		System.out.println("THIRD Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
 		System.out.println("FOURTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
 		System.out.println("FIFTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
 		System.out.println("SIXTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+		*/
 	}
 }
