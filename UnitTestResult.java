3a5c18f6ffa601b6bcad4c56a1e6d2061577f688
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-15 12:50:51 +0530

    UnitTestResult.java_(v0.001)
    UnitTestResult.java_(v0.001)

---
+++ UnitTestResult.java
@@ -0,0 +1,8 @@
+public class UnitTestResult {
+	
+	public static void main(String [] args)
+	{
+	System.out.println("This is OTHER PROJECT - UnitTest :: Program in Selenium - Git and jenkins");
+	System.out.println("This is OTHER PROJECT - UnitTest :: Last commit at 15-Jan-2020 - 12:50 PM _IST");
+	}
+}
