cf6ae1106c486e22fb1560ea4563ca90f24ebf62
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-15 12:30:10 +0530

    HelloWorld.java_(v1.03)
    HelloWorld.java_(v1.03)

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,14 +2,16 @@ public class HelloWorld {
 	
 	public static void main(String [] args)
 	{
-		System.out.println("This is First Program in Selenium - Git and jenkins");
-		System.out.println("FISRT Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		System.out.println("SECOND Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		/*
-		System.out.println("THIRD Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		System.out.println("FOURTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		System.out.println("FIFTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		System.out.println("SIXTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
-		*/
+	System.out.println("This is FIRST PROJECT - HelloWorld :: Program in Selenium - Git and jenkins");
+	System.out.println("This is FIRST PROJECT - HelloWorld :: This is First Program in Selenium - Git and jenkins");
+	System.out.println("This is FIRST PROJECT - HelloWorld :: FISRT Commit: Done in Selenium Vs Git Vs jenkins");
+	System.out.println("This is FIRST PROJECT - HelloWorld :: SECOND Commit: Done in Selenium Vs Git Vs jenkins");
+	System.out.println("This is FIRST PROJECT - HelloWorld :: Last commit at 15-Jan-2020 - 12:29 PM _IST");
+	/*
+	System.out.println("THIRD Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+	System.out.println("FOURTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+	System.out.println("FIFTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+	System.out.println("SIXTH Commit: Subject Build was Successfull and This is Done in Selenium Vs Git Vs jenkins");
+	*/
 	}
 }
