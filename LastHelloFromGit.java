a687ea80c7f1f1b52a63a026bc4848a199788d75
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-15 12:26:56 +0530

    LastHelloFromGit.java_(v3.05)
    LastHelloFromGit.java_(v3.05)

--- LastHelloFromGit.java
+++ LastHelloFromGit.java
@@ -3,7 +3,8 @@ public class LastHelloFromGit {
 	
 	public static void main(String [] args)
 	{
-    System.out.println("This is FISRT and Initial COMMIT in GitHub - HelloFromGit - Hello Jenkins From Git");
-		System.out.println("This is LAST Program - This is From Same Repository But other File");
+	System.out.println("This is THIRD PROJECT - LastHello :: This is FISRT COMMIT in GitHub - LastHelloFromGit");
+	System.out.println("This is THIRD PROJECT - LastHello :: This is LAST Program");
+	System.out.println("This is THIRD PROJECT - LastHello :: Last commite at 15-Jan-2020 12:26 IST");
 	}
   }
