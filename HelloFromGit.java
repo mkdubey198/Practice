dd7defb258df6c1274ed4ed235204a068b68b2f6
Manoranjan Dubey <55493628+mkdubey198@users.noreply.github.com>
2020-01-15 12:23:40 +0530

    HelloFromGit.java_(v1.05)
    HelloFromGit.java_(v1.05)

--- HelloFromGit.java
+++ HelloFromGit.java
@@ -4,7 +4,10 @@ public class HelloFromGit {
 	
 	public static void main(String [] args)
 	{
-		System.out.println("This is Second Program - This is From Other Repository");
-		System.out.println("This is FISRT and Initial COMMIT in GitHub - HelloFromGit - Hello Jenkins From Git");
+	System.out.println("This is SECOND PROJECT - QA");
+	System.out.println("This is SECOND PROJECT - QA :: Hello From Git");
+	System.out.println("This is SECOND PROJECT - QA :: This is From Other Repository");
+	System.out.println("This is SECOND PROJECT - QA :: This is FISRT COMMIT in GitHub - HelloFromGit - Hello Jenkins From Git");
+	System.out.println("This is SECOND PROJECT - QA :: Last Commited on 15-Jan-2020: 12:25 PM IST");
 	}
   }
