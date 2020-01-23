8f796efce361686766f9978d21ac29d16476dc57
manoranjan <manoranjan.dubey@indisoft.us>
2020-01-23 18:25:26 +0530

    k

--- src/concept/GitConcept.java
+++ src/concept/GitConcept.java
@@ -6,6 +6,7 @@ public class GitConcept {
 		System.out.println("Git Concept Discussed with NP");
 		System.out.println("Git Concept Discussed with NP");
 		System.out.println("Git Concept Discussed with NP - 3");
+		System.out.println("Git Concept Discussed with NP - 34");
 	}
 
 }
