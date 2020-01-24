46d2077a6b2f6418c8232181e6a69faf4b252862
manoranjan <manoranjan.dubey@indisoft.us>
2020-01-24 16:58:05 +0530

    k

--- src/concept_2/Concept2_File_1.java
+++ src/concept_2/Concept2_File_1.java
@@ -1,5 +1,11 @@
 package concept_2;
 
 public class Concept2_File_1 {
+	public static void main(String [] args)
+	{
+		System.out.println("Concept2_File_1");
+		System.out.println("Concept2_File_1: 24-Jan-2020: 3:28PM IST");
+		System.out.println("Concept2_File_1: 24-Jan-2020: 4:58PM IST");
+	}
 
 }
